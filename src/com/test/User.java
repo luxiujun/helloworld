651099f8a98c0be397fea6833b815b4914313f75
luxiujun <luxiujun2010@163.com>
2017-12-12 01:23:28 +0800

    123

--- src/com/test/User.java
+++ src/com/test/User.java
@@ -6,4 +6,35 @@ public class User {
   private int abc;
   private int flag;//注释//
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAbc() {
+        return abc;
+    }
+
+    public void setAbc(int abc) {
+        this.abc = abc;
+    }
+
+    public int getFlag() {
+        return flag;
+    }
+
+    public void setFlag(int flag) {
+        this.flag = flag;
+    }
 }
